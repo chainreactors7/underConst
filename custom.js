423c88b168bf31ae8d4745646acfce86df08b9d6
chainreactors7 <137114133+chainreactors7@users.noreply.github.com>
2023-08-20 22:45:30 +0530

    NWE UPDATES

--- custom.js
+++ custom.js
@@ -1,15 +1,10 @@
-const cursorSmall = document.querySelector('.small');
 const cursorBig = document.querySelector('.big');
 
-
 const positionElement = (e)=> {
-  const mouseY = e.clientY;
-  const mouseX = e.clientX;
-   
-  cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  const mouseY = e.clientY - 82;
+  const mouseX = e.clientX - 15;
   
   cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
- 
 }
 
 window.addEventListener('mousemove', positionElement)
